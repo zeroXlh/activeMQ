ef70e5a4272563cd4eccc49e5cb51401f5e538be
creat <creat@DESKTOP-HG3LS48>
2020-04-16 23:10:55 +0800

    lose

---
+++ src/main/java/com/zero/activeMQ/beanconfig/BeanConfig.java
@@ -0,0 +1,55 @@
+package com.zero.activeMQ.beanconfig;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.listener.SimpleMessageListenerContainer;
+
+@Configuration
+public class BeanConfig {
+
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("ActiveMQQueue");
+	}
+
+	@Value("${spring.activemq.broker-url}")
+	private String brokerURL;
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		return new ActiveMQConnectionFactory(ActiveMQConnectionFactory.DEFAULT_USER,
+				ActiveMQConnectionFactory.DEFAULT_PASSWORD, brokerURL);
+	}
+
+	@Bean
+	public JmsMessagingTemplate jmsMessagingTemplate(ConnectionFactory connectionFactory) {
+		return new JmsMessagingTemplate(connectionFactory);
+	}
+
+	@Bean("queueListener")
+	public JmsListenerContainerFactory<SimpleMessageListenerContainer> queueJmsListenerContainerFactory1(
+			ConnectionFactory connectionFactory) {
+		SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setPubSubDomain(false);
+		return factory;
+	}
+
+	@Bean("topicListener")
+	public JmsListenerContainerFactory<SimpleMessageListenerContainer> topicJmsListenerContainerFactory1(
+			ConnectionFactory connectionFactory) {
+		SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setPubSubDomain(true);
+		return factory;
+	}
+}
